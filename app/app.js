b60930ca8d858cabe982f641767e9e409f40f6b0
Nicola DiPalma <nickyd242@gmail.com>
2014-07-04 07:37:09 -0700

    Removed sample scripts and markup.

--- app/app.js
+++ app/app.js
@@ -18,16 +18,5 @@ angular
     'ngTouch'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+
   });
