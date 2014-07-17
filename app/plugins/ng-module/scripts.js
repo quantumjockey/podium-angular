c7cb7d0ea5ed55a5fca437aac67f66f43f1a49c8
Nicola DiPalma <nickyd242@gmail.com>
2014-07-16 17:36:59 -0700

    Began implementing module loader as directive.

--- app/plugins/ng-module/scripts.js
+++ app/plugins/ng-module/scripts.js
@@ -5,8 +5,42 @@ angular
 	.config(function () {
 
 	})
-	.directive('module', function() {
+	.directive('loadModule', [ '$document', function($document) {
+
+		var stylesheetTemplate = 	{
+				head: '<link rel="stylesheet" href="',
+				tail: '" />'
+			};
+
+		var scriptsTemplate = {
+				head: '<script src="modules/',
+				tail: '.js"></script>'
+			};
+
+		var components = [
+			'/config',
+			'/controller',
+			'/directives',
+			'/services'
+			];
+
+		function CheckIfDocExists(address) {
+			var http = new XMLHttpRequest();
+			http.open('HEAD', address, false);
+			http.send();
+			if ( http.status === 404 ){
+				return false;
+			} else {
+				return true;
+			}
+		}
+
 		return {
+			link: function(scope, element, attrs){
+
+
+
+			},
 			restrict: 'E',
 			scope: {
 				name: '@'
@@ -14,4 +48,4 @@ angular
 			templateUrl: 'plugins/ng-module/view.html',
 			transclude: false
 		};
-	});
+	}]);
